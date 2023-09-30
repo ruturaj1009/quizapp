43067098d8b7f7878fce5eb1bb9e129b06a25b3a
ruturaj1009 <ruturajsahu2002@gmail.com>
2023-09-30 20:46:56 +0530

    final code

---
+++ question.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Evaluate the expression? console.log('3' + + 3);",
+    answer: "33",
+    options: [
+      "33",
+      "6",
+      "30",
+      "66"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Evaluate the expression? console.log(3 + + '3');",
+    answer: "6",
+    options: [
+      "30",
+      "33",
+      "6",
+      "3"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Evaluate the expression? console.log('3' + '3' - '3');",
+    answer: "30",
+    options: [
+      "30",
+      "33",
+      "6",
+      "3"
+    ]
+  }
+];
